e62976ae5d021af6cb31503162d07462fd536c7d
PragyaPardhi <pardhipragya135@gmail.com>
2024-08-04 21:03:26 +0530

    Updates

---
+++ precache-manifest.0084ed82b18dfce6b83619d28771537a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164933e21c2efd9ef28a116f6353333c",
+    "url": "/Tech-Service/index.html"
+  },
+  {
+    "revision": "2f35906da8b1ba9956ab",
+    "url": "/Tech-Service/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "7196e2bd5cdbefe9becb",
+    "url": "/Tech-Service/static/css/main.a6a7c82e.chunk.css"
+  },
+  {
+    "revision": "2f35906da8b1ba9956ab",
+    "url": "/Tech-Service/static/js/2.ae1418cb.chunk.js"
+  },
+  {
+    "revision": "7196e2bd5cdbefe9becb",
+    "url": "/Tech-Service/static/js/main.8eb316d6.chunk.js"
+  },
+  {
+    "revision": "aaebb4b14604d07b93f8",
+    "url": "/Tech-Service/static/js/runtime~main.bd905549.js"
+  },
+  {
+    "revision": "4f71733319228540c4a9dee15d95e46f",
+    "url": "/Tech-Service/static/media/3539320.4f717333.jpg"
+  },
+  {
+    "revision": "40e793e7d213a77846345969de12473c",
+    "url": "/Tech-Service/static/media/Girl working on Laptop.40e793e7.mp4"
+  },
+  {
+    "revision": "8367d287e6d4963e7752dad188b9120e",
+    "url": "/Tech-Service/static/media/Work from home.8367d287.mp4"
+  },
+  {
+    "revision": "fd04fec7ca0fece61ff3182cb7c5f3f8",
+    "url": "/Tech-Service/static/media/img33.fd04fec7.jpeg"
+  }
+]);
